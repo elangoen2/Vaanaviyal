f1669cb1fa902ed8d8bf488647cb0962bd5197ed
Meikandanathan1996 <95435273+Meikandanathan1996@users.noreply.github.com>
2022-10-15 15:00:58 +0530

    Added Index.html and other startup files

--- astrology/src/main/java/com/astrologer/astrology/AstrologyApplication.java
+++ astrology/src/main/java/com/astrologer/astrology/AstrologyApplication.java
@@ -8,6 +8,8 @@ public class AstrologyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AstrologyApplication.class, args);
+		System.out.println("Hello World" +
+				);
 	}
 
 }
